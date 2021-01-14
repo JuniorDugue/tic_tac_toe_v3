7aca57bf1044e740ab9464534b4fd39f406d89fc
juniordugue <jrdugue@gmail.com>
2021-01-14 12:53:52 -0500

    bug: added extra semi colon, fixed styling

--- src/Components/styles/BoardStyled.js
+++ src/Components/styles/BoardStyled.js
@@ -8,7 +8,7 @@ export const BoardRows = styled.div`
   display: flex;
   flex-direction: row;
   border-bottom: 2px solid #000;
-  &::last-child {
+  &:last-child {
     border-bottom: none;
   }
 `;
